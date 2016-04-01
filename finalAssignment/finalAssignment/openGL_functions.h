2d8a574ee38b74fa6547845a31ee7261e8638473
ToasTer86 <duomark@gmail.com>
2016-04-01 12:58:46 +0200

    Starting point

---
+++ finalAssignment/finalAssignment/openGL_functions.h
@@ -0,0 +1,5 @@
+#include "GL/freeglut.h"
+#include <GL/gl.h>
+
+GLuint init_gl(int tex_width, int tex_height);
+void draw_quad();
---
+++ finalAssignment/finalAssignment/particleMain.cpp
@@ -0,0 +1,8 @@
+#include "opencl_utils.h"
+#include "openGL_functions.h"
+#include "stdio.h"
+
+int main()
+{
+	return 0;
+}
